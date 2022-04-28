46c51642c6e0a925a79ef4e9ee69c3ebeb46211d
arturich <arturich@gmail.com>
2022-04-28 17:39:36 -0500

    Created the with TDD the logic to create grades and InformationStudent

--- src/main/java/com/luv2code/springmvc/controller/GradebookController.java
+++ src/main/java/com/luv2code/springmvc/controller/GradebookController.java
@@ -3,16 +3,17 @@ package com.luv2code.springmvc.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.springmvc.models.CollegeStudent;
 import com.luv2code.springmvc.models.Gradebook;
+import com.luv2code.springmvc.models.GradebookCollegeStudent;
 import com.luv2code.springmvc.service.StudentAndGradeService;
 
 @Controller
@@ -20,7 +21,7 @@ public class GradebookController {
 
 	@Autowired
 	private Gradebook gradebook;
-	
+
 	@Autowired
 	private StudentAndGradeService studentService;
 
@@ -33,38 +34,109 @@ public class GradebookController {
 
 	@GetMapping("/studentInformation/{id}")
 	public String studentInformation(@PathVariable int id, Model m) {
-		return "studentInformation";
-	}
-	
-	@PostMapping("/")
-	public String createStudent(@ModelAttribute("student") CollegeStudent student,
-			Model m)
-	{
-		studentService.createStudent(student);
-		
-		Iterable<CollegeStudent> collegeStudents = studentService.getGradebook();
-		m.addAttribute("students",collegeStudents);
-		
-		return "index";
-	}
-	
-	@GetMapping("/delete/student/{id}")
-	public String deleteStudent(@PathVariable int id, Model m)
-	{
-		
-		if (!studentService.checkIfStudentIsNull(id))
-		{
-			
+
+		if (!studentService.checkIfStudentIsNull(id)) {
 			return "error";
 		}
-		
-		studentService.deleteStudent(id);
-		
-		Iterable<CollegeStudent> collegeStudents =  studentService.getGradebook();
-		
-		m.addAttribute("students",collegeStudents);
-		
+
+		GradebookCollegeStudent studentEntity = studentService.studentInformation(id);
+
+		if (studentEntity.getStudentGrades().getMathGradeResults().size() > 0) {
+			double mathAvg = studentEntity.getStudentGrades()
+					.findGradePointAverage(studentEntity.getStudentGrades().getMathGradeResults());
+			m.addAttribute("mathAverage", mathAvg);
+		} else {
+			m.addAttribute("mathAverage", "N/A");
+		}
+
+		if (studentEntity.getStudentGrades().getMathGradeResults().size() > 0) {
+			double scienceAvg = studentEntity.getStudentGrades()
+					.findGradePointAverage(studentEntity.getStudentGrades().getScienceGradeResults());
+			m.addAttribute("scienceAverage", scienceAvg);
+		} else {
+			m.addAttribute("scienceAverage", "N/A");
+		}
+		if (studentEntity.getStudentGrades().getMathGradeResults().size() > 0) {
+			double historyAvg = studentEntity.getStudentGrades()
+					.findGradePointAverage(studentEntity.getStudentGrades().getHistoryGradeResults());
+			m.addAttribute("historyAverage", historyAvg);
+		} else {
+			m.addAttribute("historyAverage", "N/A");
+		}
+
+		m.addAttribute("student", studentEntity);
+
+		return "studentInformation";
+	}
+
+	@PostMapping("/")
+	public String createStudent(@ModelAttribute("student") CollegeStudent student, Model m) {
+		studentService.createStudent(student);
+
+		Iterable<CollegeStudent> collegeStudents = studentService.getGradebook();
+		m.addAttribute("students", collegeStudents);
+
 		return "index";
+	}
+
+	@GetMapping("/delete/student/{id}")
+	public String deleteStudent(@PathVariable int id, Model m) {
+
+		if (!studentService.checkIfStudentIsNull(id)) {
+
+			return "error";
+		}
+
+		studentService.deleteStudent(id);
+
+		Iterable<CollegeStudent> collegeStudents = studentService.getGradebook();
+
+		m.addAttribute("students", collegeStudents);
+
+		return "index";
+	}
+	
+	@PostMapping("/grades")
+	public String createGrade(@RequestParam("grade") double grade,
+							  @RequestParam("gradeType") String gradeType,
+							  @RequestParam("studentId") int studentId,
+							  Model m)
+	{			
+		
+		
+		if(studentService.createGrade(grade, studentId, gradeType))
+		{
+			GradebookCollegeStudent studentEntity = studentService.studentInformation(studentId);
+			
+			m.addAttribute("student",studentEntity);
+			
+			if (studentEntity.getStudentGrades().getMathGradeResults().size() > 0) {
+				double mathAvg = studentEntity.getStudentGrades()
+						.findGradePointAverage(studentEntity.getStudentGrades().getMathGradeResults());
+				m.addAttribute("mathAverage", mathAvg);
+			} else {
+				m.addAttribute("mathAverage", "N/A");
+			}
+
+			if (studentEntity.getStudentGrades().getMathGradeResults().size() > 0) {
+				double scienceAvg = studentEntity.getStudentGrades()
+						.findGradePointAverage(studentEntity.getStudentGrades().getScienceGradeResults());
+				m.addAttribute("scienceAverage", scienceAvg);
+			} else {
+				m.addAttribute("scienceAverage", "N/A");
+			}
+			if (studentEntity.getStudentGrades().getMathGradeResults().size() > 0) {
+				double historyAvg = studentEntity.getStudentGrades()
+						.findGradePointAverage(studentEntity.getStudentGrades().getHistoryGradeResults());
+				m.addAttribute("historyAverage", historyAvg);
+			} else {
+				m.addAttribute("historyAverage", "N/A");
+			}
+			
+			return "studentInformation";
+		}
+		else
+			return "error";
 	}
 
 }
--- src/test/java/com/luv2code/springmvc/GradeBookControllerTest.java
+++ src/test/java/com/luv2code/springmvc/GradeBookControllerTest.java
@@ -38,175 +38,217 @@ import com.luv2code.springmvc.models.GradebookCollegeStudent;
 import com.luv2code.springmvc.repository.StudentDao;
 import com.luv2code.springmvc.service.StudentAndGradeService;
 
-
 @SpringBootTest
 @TestPropertySource("/application.properties")
 @AutoConfigureMockMvc
 public class GradeBookControllerTest {
-	
+
 	private static MockHttpServletRequest request;
-	
+
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
+
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@Mock
 	private StudentAndGradeService studentAndGradeServiceMock;
-	
+
 	@Autowired
 	private StudentDao studentDao;
 	
+	@Autowired
+	private StudentAndGradeService studentAndGradeService;
+
 	@Value("${sql.scripts.create.student}")
 	String sqlCreateStudent;
-	
+
 	@Value("${sql.scripts.create.math.grade}")
 	String sqlMathGrade;
-	
+
 	@Value("${sql.scripts.create.science.grade}")
 	String sqlScienceGrade;
-	
+
 	@Value("${sql.scripts.create.history.grade}")
 	String sqlHistoryGrade;
-	
+
 	@Value("${sql.script.delete.student}")
 	String sqlDeleteStudent;
-	
+
 	@Value("${sql.script.delete.math.grade}")
 	String sqlDeleteMath;
-	
+
 	@Value("${sql.script.delete.science.grade}")
 	String sqlDeleteScience;
-	
+
 	@Value("${sql.script.delete.history.grade}")
 	String sqlDeleteHistory;
-	
-	
+
 	@BeforeAll
-	public static void setup()
-	{
+	public static void setup() {
 		request = new MockHttpServletRequest();
 		request.setParameter("firstname", "Chad");
 		request.setParameter("lastname", "Darby");
 		request.setParameter("emailAddress", "chad.darby@luv2code_school.com");
 	}
-	
-	
+
 	@BeforeEach
-	public void setupDatabase()
-	{
-		jdbcTemplate.execute(sqlCreateStudent);		
+	public void setupDatabase() {
+		jdbcTemplate.execute(sqlCreateStudent);
 		jdbcTemplate.execute(sqlMathGrade);
 		jdbcTemplate.execute(sqlScienceGrade);
 		jdbcTemplate.execute(sqlHistoryGrade);
 	}
-	
+
 	@Test
 	@DisplayName("Student Http Request")
-	public void getSTudentHtttpRequest() throws Exception
-	{
-		CollegeStudent studentOne = new GradebookCollegeStudent("Eric", 
-				"Petters", "eric@citalin.com" );
-		
-		CollegeStudent studentTwo = new GradebookCollegeStudent("Arthur", 
-				"Kings", "artur.kings@citalin.com" );
-		
-		List<CollegeStudent> collegeStudentsList = new ArrayList<>(Arrays.asList(studentOne,studentTwo));
-		
-		when(studentAndGradeServiceMock.getGradebook())
-			.thenReturn(collegeStudentsList);
-		
+	public void getSTudentHtttpRequest() throws Exception {
+		CollegeStudent studentOne = new GradebookCollegeStudent("Eric", "Petters", "eric@citalin.com");
+
+		CollegeStudent studentTwo = new GradebookCollegeStudent("Arthur", "Kings", "artur.kings@citalin.com");
+
+		List<CollegeStudent> collegeStudentsList = new ArrayList<>(Arrays.asList(studentOne, studentTwo));
+
+		when(studentAndGradeServiceMock.getGradebook()).thenReturn(collegeStudentsList);
+
 		assertIterableEquals(collegeStudentsList, studentAndGradeServiceMock.getGradebook());
-		
-		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/"))
-				.andExpect(status().isOk()).andReturn();
-		
+
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(status().isOk()).andReturn();
+
 		ModelAndView mav = mvcResult.getModelAndView();
-		
+
 		ModelAndViewAssert.assertViewName(mav, "index");
 	}
-	
-	
+
 	@Test
 	@DisplayName("Create Student via http request")
-	public void createStudentHttpRequest() throws Exception
-	{
-		CollegeStudent studentOne = new GradebookCollegeStudent("Eric", 
-				"Petters", "eric@citalin.com" );
-		
+	public void createStudentHttpRequest() throws Exception {
+		CollegeStudent studentOne = new GradebookCollegeStudent("Eric", "Petters", "eric@citalin.com");
+
 		List<CollegeStudent> collegeStudentList = new ArrayList<>(Arrays.asList(studentOne));
-		
+
 		when(studentAndGradeServiceMock.getGradebook()).thenReturn(collegeStudentList);
-		
+
 		assertIterableEquals(collegeStudentList, studentAndGradeServiceMock.getGradebook());
+
+		MvcResult mvcResult = (MvcResult) this.mockMvc
+				.perform(MockMvcRequestBuilders.post("/").contentType(MediaType.APPLICATION_JSON)
+						.param("firstname", request.getParameterValues("firstname"))
+						.param("lastname", request.getParameterValues("lastname"))
+						.param("emailAddress", request.getParameterValues("emailAddress")))
+				.andExpect(status().isOk()).andReturn();
+
+		ModelAndView mav = mvcResult.getModelAndView();
+
+		ModelAndViewAssert.assertViewName(mav, "index");
+
+		CollegeStudent verifyStudent = studentDao.findByEmailAddress("chad.darby@luv2code_school.com");
+
+		assertNotNull(verifyStudent, "Student should be found");
+
+		assertEquals("chad.darby@luv2code_school.com", verifyStudent.getEmailAddress(), "Exact email is expected");
+	}
+
+	@Test
+	@DisplayName("Delete an student")
+	public void deleteStudentHttpRequest() throws Exception {
+		int id = 1;
+		// Get student first
+		Optional<CollegeStudent> verifyStudent = studentDao.findById(id);
+		assertTrue(verifyStudent.isPresent(), "Student should exist before removing it");
+
+		// Delete from controller
+		MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get("/delete/student/{id}", id))
+				.andExpect(status().isOk()).andReturn();
+
+		ModelAndView mav = mvcResult.getModelAndView();
+
+		ModelAndViewAssert.assertViewName(mav, "index");
+
+		verifyStudent = studentDao.findById(id);
+		assertFalse(verifyStudent.isPresent(), "Student should NOT exist before removing it");
+
+	}
+
+	@Test
+	@DisplayName("Error page for student that does not exist")
+	public void errorPageOnDelete() throws Exception {
+		int id = 2;
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/delete/student/{id}", id))
+				.andExpect(status().isOk()).andReturn();
+
+		ModelAndView mav = mvcResult.getModelAndView();
+
+		ModelAndViewAssert.assertViewName(mav, "error");
+
+	}
+
+	@Test
+	@DisplayName("Get student information http resquest")
+	public void getStudentInformationHttpRequest() throws Exception {
+		int id = 1;
+
+		assertTrue(studentDao.findById(id).isPresent(), "Student must exist");
+
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/studentInformation/{id}", id))
+				.andExpect(status().isOk()).andReturn();
+
+		ModelAndView mav = mvcResult.getModelAndView();
+
+		ModelAndViewAssert.assertViewName(mav, "studentInformation");
+
+	}
+
+	@Test
+	@DisplayName("Error page at wrong Student Information id")
+	public void errorPageAtStudenInformation() throws Exception {
+		int id = 0;
+
+		assertFalse(studentDao.findById(id).isPresent());
+
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/studentInformation/{id}", id))
+				.andExpect(status().isOk()).andReturn();
+
+		ModelAndView mav = mvcResult.getModelAndView();
+
+		ModelAndViewAssert.assertViewName(mav, "error");
+	}
+
+	@Test
+	@DisplayName("Add/Create grades")
+	public void addGrades() throws Exception
+	{
 		
-		MvcResult mvcResult = (MvcResult) this.mockMvc.perform(MockMvcRequestBuilders.post("/")
+		assertTrue(studentDao.findById(1).isPresent());		
+				
+		GradebookCollegeStudent student = studentAndGradeService.studentInformation(1);
+		
+		assertEquals(1,student.getStudentGrades().getMathGradeResults().size());
+		
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/grades")
 				.contentType(MediaType.APPLICATION_JSON)
-				.param("firstname", request.getParameterValues("firstname"))
-				.param("lastname", request.getParameterValues("lastname"))
-				.param("emailAddress", request.getParameterValues("emailAddress"))
+				.param("gradeType", "math")
+				.param("studentId", "1")
+				.param("grade", "100")
 				).andExpect(status().isOk()).andReturn();
 		
 		ModelAndView mav = mvcResult.getModelAndView();
 		
-		ModelAndViewAssert.assertViewName(mav, "index");
+		ModelAndViewAssert.assertViewName(mav, "studentInformation");
 		
-		CollegeStudent verifyStudent = studentDao.findByEmailAddress("chad.darby@luv2code_school.com");
+		student = studentAndGradeService.studentInformation(1);
+		assertEquals(2,student.getStudentGrades().getMathGradeResults().size());
 		
-		assertNotNull(verifyStudent,"Student should be found");
-		
-		assertEquals("chad.darby@luv2code_school.com", verifyStudent.getEmailAddress(),"Exact email is expected");
-	}
-	
-	@Test
-	@DisplayName("Delete an student")
-	public void deleteStudentHttpRequest() throws Exception
-	{
-		int id = 1;
-		//Get student first 
-		Optional<CollegeStudent> verifyStudent = studentDao.findById(id);
-		assertTrue(verifyStudent.isPresent(),"Student should exist before removing it");
-		
-		//Delete from controller
-		MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get("/delete/student/{id}",id))
-				.andExpect(status().isOk()).andReturn();
-		
-		ModelAndView mav = mvcResult.getModelAndView();
-		
-		ModelAndViewAssert.assertViewName(mav, "index");
-		
-		verifyStudent = studentDao.findById(id);
-		assertFalse(verifyStudent.isPresent(),"Student should NOT exist before removing it");
 		
 		
 	}
-	
-	@Test
-	@DisplayName("Error page for student that does not exist")
-	public void errorPageOnDelete() throws Exception
-	{
-		int id = 2;
-		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/delete/student/{id}",id))
-				.andExpect(status().isOk()).andReturn();
-		
-		ModelAndView mav = mvcResult.getModelAndView();
-		
-		ModelAndViewAssert.assertViewName(mav, "error");		
-		
-	}
-	
-	
+
 	@AfterEach
-	public void setupAfterTransaction()
-	{
+	public void setupAfterTransaction() {
 		jdbcTemplate.execute(sqlDeleteStudent);
 		jdbcTemplate.execute(sqlDeleteMath);
 		jdbcTemplate.execute(sqlDeleteScience);
 		jdbcTemplate.execute(sqlDeleteHistory);
 	}
-	
-	
-	
 
 }
